c9193ef1eed621abab1dc3bc1ab362266b0d0940
Tushar Aggarwal <tushar.inseec@gmail.com>
2023-04-03 16:25:31 +0530

    Update Boston_app.py

--- Boston_app.py
+++ Boston_app.py
@@ -1,4 +1,4 @@
-# Boston House Price Prediction Application
+# California House Price Prediction Application
 
 # Importing the required libraries
 import streamlit as st
@@ -6,17 +6,17 @@ import pandas as pd
 import numpy as np
 import shap
 import matplotlib.pyplot as plt
-from sklearn import datasets
+from sklearn.datasets import fetch_california_housing
 from sklearn.ensemble import RandomForestRegressor
 
 # Application body
-st.title("Boston House Price Prediction Application")
-st.write(" This application predicts the Boston House Prices")
+st.title("California House Price Prediction Application")
+st.write(" This application predicts the California House Prices")
 # Loading the dataset
-boston = datasets.load_boston()
+housing = fetch_california_housing()
 # Defining feature and target variable
-X=pd.DataFrame(boston.data, columns=boston.feature_names)
-y=pd.DataFrame(boston.target, columns=['MEDV'])
+X=pd.DataFrame(housing.data, columns=housing.feature_names)
+y=pd.DataFrame(housing.target, columns=['MedHouseVal'])
 
 # Sidebar
 st.sidebar.header("Please specify input parameters")
@@ -24,33 +24,22 @@ st.sidebar.header("Please specify input parameters")
 # Defining user_input_features
 
 def user_input_features():
-    CRIM =st.sidebar.slider("CRIM", X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())# Format .slider("",min val, max val , default val)
-    ZN =st.sidebar.slider("ZN", X.ZN.min(), X.ZN.max(), X.ZN.mean())
-    INDUS =st.sidebar.slider("INDUS", X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
-    CHAS =st.sidebar.slider("CHAS", X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
-    NOX =st.sidebar.slider("NOX", X.NOX.min(), X.NOX.max(), X.NOX.mean())
-    RM =st.sidebar.slider("RM", X.RM.min(), X.RM.max(), X.RM.mean())
-    AGE =st.sidebar.slider("AGE", X.AGE.min(), X.AGE.max(), X.AGE.mean())
-    DIS =st.sidebar.slider("DIS", X.DIS.min(), X.DIS.max(), X.DIS.mean())
-    RAD =st.sidebar.slider("RAD", X.RAD.min(), X.RAD.max(), X.RAD.mean())
-    TAX =st.sidebar.slider("TAX", X.TAX.min(), X.TAX.max(), X.TAX.mean())
-    PTRATIO =st.sidebar.slider("PTRATIO", X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
-    B =st.sidebar.slider("B", X.B.min(), X.B.max(), X.B.mean())
-    LSTAT =st.sidebar.slider("LSTAT", X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
+    MedInc =st.sidebar.slider("MedInc", X.MedInc.min(), X.MedInc.max(), X.MedInc.mean())# Format .slider("",min val, max val , default val)
+    HouseAge =st.sidebar.slider("HouseAge", X.HouseAge.min(), X.HouseAge.max(), X.HouseAge.mean())
+    AveRooms =st.sidebar.slider("AveRooms", X.AveRooms.min(), X.AveRooms.max(), X.AveRooms.mean())
+    Population =st.sidebar.slider("Population", X.Population.min(), X.Population.max(), X.Population.mean())
+    AveOccup =st.sidebar.slider("AveOccup", X.AveOccup.min(), X.AveOccup.max(), X.AveOccup.mean())
+    Latitude =st.sidebar.slider("Latitude", X.Latitude.min(), X.Latitude.max(), X.Latitude.mean())
+    Longitude =st.sidebar.slider("Longitude", X.Longitude.min(), X.Longitude.max(), X.Longitude.mean())
     
-    data = {'CRIM': CRIM,
-            'ZN': ZN,
-            'INDUS': INDUS,
-            'CHAS': CHAS,
-            'NOX': NOX,
-            'RM': RM,
-            'AGE': AGE,
-            'DIS': DIS,
-            'RAD': RAD,
-            'TAX': TAX,
-            'PTRATIO': PTRATIO,
-            'B': B,
-            'LSTAT': LSTAT}
+    
+    data = {'MedInc': MedInc,
+            'HouseAge': HouseAge,
+            'AveRooms': AveRooms,
+            'Population': Population,
+            'AveOccup': AveOccup,
+            'Latitude': Latitude,
+            'Longitude': Longitude}
     
     features =pd.DataFrame(data, index=[0])
     return features
@@ -71,7 +60,7 @@ model.fit(X, y)
 predictions =model.predict(df)
 
 # Predication display on App
-st.header("Predication of MEDV")
+st.header("Predication of MedHouseVal")
 st.write(predictions)
 st.write("---")
 
